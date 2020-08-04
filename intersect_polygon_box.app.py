57fb21db8b00c3acf684560d2f7439c952fe4c0e
wooruang <wooruang@gmail.com>
2020-08-04 13:19:21 +0900

    Fix the bug in input's boxes

--- intersect_polygon_box.app.py
+++ intersect_polygon_box.app.py
@@ -24,6 +24,7 @@ def on_get(k):
 
 def on_run(boxes, polygons):
     # sys.stdout.write(f"[intersect_polygon_box.on_run] boxes    :{boxes}\n")
+    # sys.stdout.write(f"[intersect_polygon_box.on_run] boxes.size : {boxes.size}\n")
     # sys.stdout.write(f"[intersect_polygon_box.on_run] polygons : {polygons}\n")
     # sys.stdout.write(f"[intersect_polygon_box.on_run] polygons.size : {polygons.size}\n")
     # sys.stdout.flush()
@@ -33,6 +34,9 @@ def on_run(boxes, polygons):
             'intersect': None,
             'remain': boxes
         }
+
+    if boxes.size < 4:
+        return {'intersect': None, 'remain': None}
 
     points = [intersect.get_point_of_box(x, point_type) for x in boxes]
     # sys.stdout.write(f"[intersect_polygon_box.on_run] point type: {point_type}\n")
